9e60e59e4bf45ccfc4e4f7c15b065a92b45ae401
MoMathAcademy <farshad.mohebban@gmail.com>
2026-07-30 06:53:40 +0000

    new prem

--- mo-math-academy/src/app/components/Hero.tsx
+++ mo-math-academy/src/app/components/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero() {
             <br />
             <span className="text-green-700">Confidence</span> in Math!
             <br />
-            
+
             One Student
             <br />
             at a Time!
@@ -54,7 +54,7 @@ export default function Hero() {
               -right-56
               top-1/2
               h-[900px]
-              w-[1030px]
+              w-[1010px]
               -translate-y-1/2
               rounded-l-[900px]
               bg-[#0F8A42]
